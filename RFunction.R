2706674a6344684fe49874a17c57185c6efc0232
janhanz <honzahanzelka@gmail.com>
2025-02-19 13:57:27 +0100

    The output CSV files are exported also when bird_ID incudes non-ASCII characters.
    Row numbers in First_row and Last_Row in Classification_records_summary match with the row numbers in the main output.

--- RFunction.R
+++ RFunction.R
@@ -12,6 +12,7 @@ library('purrr')
 library('sf')
 library('stringr')
 library('tidyr')
+library('stringi')
 
 rFunction = function(data, cap_status = NULL, nest_coords = NULL, single_blk_merge = TRUE, gr_speed = FALSE, bursts_rec = FALSE,
                      max_flight_sp = 40, dst_stat_gap = 30, time_stat_gap = 3, centr_dist = 50, near_blk_dist = 20, near_stop_rec = 1, 
@@ -647,8 +648,8 @@ rFunction = function(data, cap_status = NULL, nest_coords = NULL, single_blk_mer
   
   # recalculate the metrics in summary output
   tab_sum_stat_sep_rev <- dt_export_sep_rev %>%
-    filter(Block_type == "stationary") %>% 
     mutate("row_nb" = as.numeric(rownames(.))) %>% 
+    filter(Block_type == "stationary") %>% 
     st_as_sf(., coords = c("Long", "Lat"), crs = "EPSG:4326", remove = FALSE) %>% 
     group_by(Block_nr, Block_type, Block_class) %>%
     summarise(Start_date = min(timestamp),
@@ -757,11 +758,13 @@ rFunction = function(data, cap_status = NULL, nest_coords = NULL, single_blk_mer
     rename(any_of(c(Speed_gr = "ground_speed", Timestamp = "timestamp"))) %>% 
     select(any_of(c("Bird_ID", "Block_nr", "Block_type", "Block_class", "Block_size", "Dist_consec", "Speed_consec", "Speed_gr", "Year", "Month", "Day", "Hour", "Minute", "Second", "Timestamp", "Lat", "Long")))
   
-
+  # transliterate the bird_ID to ASCII, to prevent problems during the export of CSV files
+  bird_ID_ASCII <- stri_trans_general(bird_ID, "Latin-ASCII")
+  
   # write the final output and summary output
-  write.csv(tab_sum_stat_sep_rev, file = appArtifactPath(paste0("Classification_records_summary_", bird_ID, ".csv")), row.names = FALSE)
-  write.csv(dt_export_sep_rev, file = appArtifactPath(paste0("Classification_records_", bird_ID, ".csv")), row.names = FALSE)
-
+  write.csv(tab_sum_stat_sep_rev, file = appArtifactPath(paste0("Classification_records_summary_", bird_ID_ASCII, ".csv")), row.names = FALSE)
+  write.csv(dt_export_sep_rev, file = appArtifactPath(paste0("Classification_records_", bird_ID_ASCII, ".csv")), row.names = FALSE)
+  
   return(data_out)
 }
 
